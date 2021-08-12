ea6f7262912e58e6d3e9e6da77536d86b9b0da8b
ozanakyel <69152994+ozanakyel@users.noreply.github.com>
2021-08-12 11:25:54 +0300

    added thread client

---
+++ client_thread.py
@@ -0,0 +1,52 @@
+from pyModbusTCP.client import ModbusClient
+from time import sleep
+from threading import Thread
+from random import uniform
+
+
+class ClientThreading(Thread):
+    """This is a client with threading for ModBusTCP communication."""
+
+    def __init__(self):
+        Thread.__init__(self)
+        self.host = "127.0.0.1"
+        self.port = 502  # 502
+        self.client = ModbusClient(self.host, self.port)
+        print(f"hedef {self.host} ve {self.port}")
+        self.register_sifir = None
+        self.register_1 = None
+        self.register_2 = None
+        self.register_3 = None
+        self.register_4 = None
+        self.register_5 = None
+        self.register_6 = None
+        self.register_sifir_old = 0
+
+    def run(self):
+        while True:
+            if self.client.open():
+                # print("client canli")
+                self.register_sifir = self.client.read_holding_registers(0)
+                self.register_1 = self.client.read_holding_registers(1)
+                self.register_2 = self.client.read_holding_registers(2)
+                self.register_3 = self.client.read_holding_registers(3)
+                self.register_4 = self.client.read_holding_registers(4)
+                self.register_5 = self.client.read_holding_registers(5)
+                self.register_6 = self.client.read_holding_registers(6)
+                # print(f"run:{self.register_sifir}")
+                # client.write_single_register(1, int(uniform(1, 100)))
+            else:
+                print("baglanti kurulamadi")
+            # sleep(2)
+
+    def get_registersifir(self):
+        while True:
+            if self.register_sifir != self.register_sifir_old:
+                self.register_sifir_old = self.register_sifir
+                return [self.register_sifir,
+                        self.register_1,
+                        self.register_2,
+                        self.register_3,
+                        self.register_4,
+                        self.register_5,
+                        self.register_6]
--- modbus_server.py
+++ modbus_server.py
@@ -5,20 +5,26 @@ from pyModbusTCP.server import ModbusServer, DataBank
 host = "127.0.0.1"
 port = 502
 server = ModbusServer(host, port, no_block=True)
-
+spec = ["corolla_black", "corolla_white", "chr_balck", "chr_white"]
 try:
     print("server is starting....")
     server.start()
     print(f"server IP: {host} {port}")
     state = [0]
     while True:
-        DataBank.set_words(0, [int(uniform(0,100))])
-        if state != DataBank.get_words(1):
-            state = DataBank.get_words(1)
-            print(f"register 1'daki deger {state} olarak degisti")
-            sleep(0.5)
-            #deneme git klone
-except:
+        DataBank.set_words(0, [int(uniform(0, 1000))])       # AssyNo
+        DataBank.set_words(1, [int(uniform(0, 1000))])       # BodyNo(first 3 digits)
+        DataBank.set_words(2, [int(uniform(0, 100))])        # BodyNo(last 2 digits)
+        DataBank.set_words(3, [int(1)])                     # Start point
+        DataBank.set_words(4, [int(1)])                     # check point
+        DataBank.set_words(5, [int(1)])                     # Complete point
+        DataBank.set_words(6, [int(uniform(0, 4))])         # Spec info
+        # if state != DataBank.get_words(1):
+        #     state = DataBank.get_words(1)
+        #     print(f"register 1'daki deger {state} olarak degisti")
+        sleep(3)
+except Exception as e:
+    print(e)
     print("server stoplandi")
     server.stop()
     print("server devre disi")
